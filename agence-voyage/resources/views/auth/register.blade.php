683e18e50dc0a3709ab82f1a22392b2cbcaac858
Kesmaecker Thibaut <thibaut@mister-e.fr>
2022-05-18 09:04:28 +0200

    fix bg page register

--- agence-voyage/resources/views/auth/register.blade.php
+++ agence-voyage/resources/views/auth/register.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="full-bg">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -74,4 +75,5 @@
         </div>
     </div>
 </div>
+<div class="full-bg">
 @endsection
