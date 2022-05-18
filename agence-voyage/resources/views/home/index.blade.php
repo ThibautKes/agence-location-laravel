78a293dca56c34d55508eff071bcb7801c502166
Kesmaecker Thibaut <thibaut@mister-e.fr>
2022-05-18 13:17:49 +0200

    fix form home

--- agence-voyage/resources/views/home/index.blade.php
+++ agence-voyage/resources/views/home/index.blade.php
@@ -15,28 +15,28 @@
             <h3 class="sub-title">Selectionnez votre aéroport de départ</h3>
             <div class="flex-line">
                 <div>
-                    <select name="aeroport1" id="aeroport1">
+                    <select name="aeroport1" id="aeroport1" class="inputhome">
                         @foreach($aeroports as $aeroport)
                             <option value={{$aeroport->id}}>{{$aeroport->nom}} - {{$aeroport->ville}}</option>
                         @endforeach
                     </select>
                 </div>
                 <label for="date1">Date départ
-                    <input type="date" name="date1">
+                    <input type="date" name="date1" class="inputhome">
                 </label>
             </div>
 
             <h3 class="sub-title2">Selectionnez votre aéroport de retour</h3>
             <div class="flex-line">
                 <div>
-                    <select name="aeroport2" id="aeroport2">
+                    <select name="aeroport2" id="aeroport2" class="inputhome">
                         @foreach($aeroports as $aeroport)
                             <option value={{$aeroport->id}} >{{$aeroport->nom}} - {{$aeroport->ville}}</option>
                         @endforeach
                     </select>
                 </div>
                 <label for="date2">Date retour
-                    <input type="date" name="date2" id="date2">
+                    <input type="date" name="date2" id="date2" class="inputhome">
                 </label>
             </div>
             @if($message != null)
